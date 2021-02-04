55662cee9cf256109467bcf56a14ae6830758214
Isaiah Weaver <61598180+Caspian-X@users.noreply.github.com>
2021-02-04 14:41:38 -0800

    Running Animation
    In IsaiahFunPlace folder

--- Assets/IsaiahFunPlace/Scripts/FirstMovements.cs
+++ Assets/IsaiahFunPlace/Scripts/FirstMovements.cs
@@ -2,32 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class FirstMovements : MonoBehaviour
 {
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+            animator.SetBool("isRunning", false);
+
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * 20, Camera.main.transform);
+            transform.Translate(Vector3.forward * Time.deltaTime * 20);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * 20, Camera.main.transform);
+            transform.Translate(Vector3.left * Time.deltaTime * 20);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * Time.deltaTime * 20, Camera.main.transform);
+            transform.Translate(Vector3.back * Time.deltaTime * 20);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * 20, Camera.main.transform);
+            transform.Translate(Vector3.right * Time.deltaTime * 20);
         }
 
     }
