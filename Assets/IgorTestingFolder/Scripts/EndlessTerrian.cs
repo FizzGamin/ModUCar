a70f0b45b7a5f7d46ca396fc4b828bcd16411fda
Optimum001 <44273455+Optimum001@users.noreply.github.com>
2021-02-05 19:40:37 -0800

    Improved fps

--- Assets/IgorTestingFolder/Scripts/EndlessTerrian.cs
+++ Assets/IgorTestingFolder/Scripts/EndlessTerrian.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EndlessTerrian : MonoBehaviour
 {
 
-    const float scale = 5f;
+    const float scale = 2f;
 
     const float viewerMoveThresholdForChunkUpdate = 25f;
     const float sqrViewerMoveThresholdForChunkUpdate = viewerMoveThresholdForChunkUpdate * viewerMoveThresholdForChunkUpdate;
@@ -91,6 +91,7 @@ public class EndlessTerrian : MonoBehaviour
 
         LODInfo[] detailLevels;
         LODMesh[] lodMeshes;
+        LODMesh collisionLODMesh;
 
         MapData mapData;
         bool mapDataReceived;
@@ -119,6 +120,10 @@ public class EndlessTerrian : MonoBehaviour
             for(int i = 0; i < detailLevels.Length; i++)
             {
                 lodMeshes[i] = new LODMesh(detailLevels[i].lod, UpdateTerrainChunk);
+                if (detailLevels[i].useForCollider)
+                {
+                    collisionLODMesh = lodMeshes[i];
+                }
             }
 
             mapGenerator.RequestMapData(position, OnMapDataReceived);
@@ -160,10 +165,20 @@ public class EndlessTerrian : MonoBehaviour
                         {
                             previousLODIndex = lodIndex;
                             meshFilter.mesh = lodMesh.mesh;
-                            meshCollider.sharedMesh = lodMesh.mesh;
                         }
                         else if (!lodMesh.hasRequestedMesh)
                             lodMesh.RequestMesh(mapData);
+                    }
+                    if(lodIndex == 0)
+                    {
+                        if (collisionLODMesh.hasMesh)
+                        {
+                            meshCollider.sharedMesh = collisionLODMesh.mesh;
+                        }
+                        else if (!collisionLODMesh.hasRequestedMesh)
+                        {
+                            collisionLODMesh.RequestMesh(mapData);
+                        }
                     }
                     terrainChunksVisibleLastUpdate.Add(this);
                 }
@@ -217,6 +232,7 @@ public class EndlessTerrian : MonoBehaviour
     {
         public int lod;
         public float visibleDstThreshold;
+        public bool useForCollider;
     }
 
 }
