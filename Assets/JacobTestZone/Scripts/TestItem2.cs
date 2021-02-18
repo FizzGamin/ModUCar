2260e9beb2dcc12897f55e0502ce434bdac6aa57
jacoberdman2147 <null@null>
2021-02-18 14:59:17 -0800

    Adding a commit with all my inventory and sprite stuff but it\'s WIP

--- Assets/JacobTestZone/Scripts/TestItem2.cs
+++ Assets/JacobTestZone/Scripts/TestItem2.cs
@@ -1,58 +1,20 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TestItem2 : IItem, IInteractable
+public class TestItem2 : IPickupable
 {
-    private const string ENABLED_TEXT = "Turn off";
-    private const string DISABLED_TEXT = "Turn on";
-
-    public int count = 20;
-    public int radius = 10;
-    public float rotSpeed = 1;
-
-    private float rot = 0;
-    private List<GameObject> spheres = new List<GameObject>();
-    private bool isEnabled = false;
-
-    // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < count; i++)
-        {
-            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            sphere.SetActive(false);
-            spheres.Add(sphere);
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isEnabled) return;
-
-        for (int i = 0; i < spheres.Count; i++)
+        if (Input.GetKeyDown("p"))
         {
-            float offset = (360 / count * i) + rot;
-            Transform t = spheres[i].transform;
-            t.position = transform.position + new Vector3(0, (float)Math.Cos(rad(offset)) * radius, (float)Math.Sin(rad(offset)) * radius);
+            Debug.Log("Script is active");
         }
-        rot += rotSpeed;
-    }
-
-    private float rad(float degrees)
-    {
-        return (float)(degrees * Math.PI) / 180;
-    }
-
-    public void Interact(IPlayer player)
-    {
-        Debug.Log("Interacted");
-    }
-
-    public string GetInteractionText()
-    {
-        return isEnabled ? ENABLED_TEXT : DISABLED_TEXT;
     }
 
     public override GameObject CreateItem()
@@ -73,5 +35,10 @@ public class TestItem2 : IItem, IInteractable
     public override int GetWeight()
     {
         return 100;
+    }
+
+    public override Sprite GetSprite()
+    {
+        return null;
     }
 }
