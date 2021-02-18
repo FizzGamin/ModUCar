0533fa93b958989c5c34317670cead8823e302f2
FizzGaming <gri108@yahoo.com>
2021-02-18 13:09:12 -0800

    Imported TMP and added new testing scene for driving the car
    The car also now has much better driving and fixed visual glitch with the wheel turning

---
+++ Assets/TestingSetups/ModularVehicleDriving/DriveTest/Scripts/DriveTestController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveTestController : MonoBehaviour
+{
+    //Prefabs
+    public GameObject _1x1CarPrefab;
+    public GameObject _2x1CarPrefab;
+    public GameObject _3x1CarPrefab;
+    public GameObject _4x1CarPrefab;
+    public GameObject cameraPrefab;
+
+    private GameObject curCar;
+
+    void DestroyOldCar()
+    {
+        Destroy(curCar);
+    }
+
+    void CreateCamera()
+    {
+        if(Camera.main)
+            Destroy(Camera.main.gameObject);
+
+        Transform camera = Instantiate(cameraPrefab, Vector3.zero, Quaternion.identity).transform;
+        camera.SetParent(curCar.transform);
+        camera.localPosition = new Vector3(0, 25, -80);
+        camera.localEulerAngles = new Vector3(10, 0, 0);
+    }
+
+    public void Spawn1x1Car()
+    {
+        DestroyOldCar();
+        curCar = Instantiate(_1x1CarPrefab, Vector3.zero, Quaternion.identity);
+        CreateCamera();
+    }
+    public void Spawn2x1Car()
+    {
+        DestroyOldCar();
+        curCar = Instantiate(_2x1CarPrefab, Vector3.zero, Quaternion.identity);
+        CreateCamera();
+    }
+    public void Spawn3x1Car()
+    {
+        DestroyOldCar();
+        curCar = Instantiate(_3x1CarPrefab, Vector3.zero, Quaternion.identity);
+        CreateCamera();
+    }
+    public void Spawn4x1Car()
+    {
+        DestroyOldCar();
+        curCar = Instantiate(_4x1CarPrefab, Vector3.zero, Quaternion.identity);
+        CreateCamera();
+    }
+}
