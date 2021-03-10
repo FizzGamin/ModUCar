0dac37b08aafc3dff25b6ad1a59c63d65cea3c0a
Isaiah Weaver <61598180+Caspian-X@users.noreply.github.com>
2021-03-10 14:13:52 -0800

    .

---
+++ Assets/Scripts/Enemy/Spider/SpiderAI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiderAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(new Vector3(Random.Range(25f, 100f), 0, Random.Range(25f, 100f)));
+        transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
