0942a0fcd6089d0303450fc720f72def9d5bb395
Isaiah Weaver <61598180+Caspian-X@users.noreply.github.com>
2021-04-07 17:54:46 -0700

    stars skybox

--- Assets/Scripts/WorldGeneration/sun.cs
+++ Assets/Scripts/WorldGeneration/sun.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class sun : MonoBehaviour
 {
+    public Material day;
+    public Material night;
+    private Transform sunTransform;
+    private float rotationSpeed = 1;
+
     public Transform player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sunTransform = GameObject.Find("Sun").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Vector3.zero, Vector3.right, 10f * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.right, rotationSpeed * Time.deltaTime);
         transform.LookAt(Vector3.zero);
+
+        if (sunTransform.position.y > 0)
+        {
+            RenderSettings.skybox = day;
+            rotationSpeed = 5f / 3;
+            sunTransform.gameObject.GetComponent<Light>().intensity = 0.5f;
+        }
+        else
+        {
+            RenderSettings.skybox = night;
+            rotationSpeed = 15f / 3;
+            sunTransform.gameObject.GetComponent<Light>().intensity = 0f;
+        }
     }
 }
