85ee455d34701b9cef49422e9634a92bd2f6995e
Isaiah Weaver <61598180+Caspian-X@users.noreply.github.com>
2021-04-29 13:20:26 -0700

    Finished the turret

--- Assets/Scripts/Module/TurretModule.cs
+++ Assets/Scripts/Module/TurretModule.cs
@@ -53,7 +53,7 @@ public class TurretModule : VehicleModule
 
         if (enemyInSightRange)
         {
-            Vector3 enemyPos = findNearestEnemy();
+            Vector3 enemyPos = FindNearestEnemy();
             if (enemyPos != null)
                 Attack(enemyPos);
         }
@@ -65,28 +65,35 @@ public class TurretModule : VehicleModule
     /// <param name="enemyPos">The position of the enemy to attack.</param>
     private void Attack(Vector3 enemyPos)
     {
-        Debug.Log(enemyPos);
         Vector3 topLook = new Vector3(enemyPos.x, top.transform.position.y, enemyPos.z);
-        Vector3 turretLook = new Vector3(enemyPos.x, checkValue(enemyPos.y, 30), enemyPos.z);
+        Vector3 turretLook = new Vector3(enemyPos.x, CheckValue(enemyPos.y, enemyPos, 30), enemyPos.z);
         top.transform.LookAt(topLook);
         turret.transform.LookAt(turretLook);
+
         //SHOOT
         attacked = true;
-        Invoke(nameof(ResetAttack), 1f);
+        Invoke(nameof(ResetAttack), 2f);
     }
 
-    private float checkValue(float val, float max)
+    private float CheckValue(float pos, Vector3 enemyPos, float maxAngle)
     {
-        if (val > max)
-            return max;
-        return val;
+        Vector3 flatAngle = new Vector3(enemyPos.x, turret.transform.position.y, enemyPos.z);
+        Vector3 tiltAngle = new Vector3(enemyPos.x, enemyPos.y, enemyPos.z);
+        float angle = Vector3.SignedAngle(flatAngle, tiltAngle, Vector3.forward);
+        Debug.Log(angle);
+        if (angle > 80)
+            return flatAngle.y;
+        else if (angle < -30)
+            return flatAngle.y;
+        else
+            return enemyPos.y;
     }
 
     /// <summary>
     /// Finds the nearest enemy to this turret module.
     /// </summary>
     /// <returns>The closest enemy's position, null if there is no closest enemy.</returns>
-    private Vector3 findNearestEnemy()
+    private Vector3 FindNearestEnemy()
     {
         GameObject[] spiders = GameObject.FindGameObjectsWithTag("Enemy_Spider");
         GameObject[] trucks = GameObject.FindGameObjectsWithTag("Enemy_Truck");
