ad24fd7534c56b0d55410c683859796402614822
Isaiah Weaver <61598180+Caspian-X@users.noreply.github.com>
2021-05-04 14:33:56 -0700

    Lots of small changes. Moved some files, changed player colliders, lazer gun stuff.

--- Assets/Scripts/Item/weapons/LazerShoot.cs
+++ Assets/Scripts/Item/weapons/LazerShoot.cs
@@ -6,18 +6,6 @@ public class LazerShoot : MonoBehaviour
 {
     RaycastHit hit;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ShootRay(GameObject shotPrefab, float range)
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
